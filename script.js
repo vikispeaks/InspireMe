d6d4303e7bce71fbf81d630cd8b3ede993742118
Vignesh Rajendran <vign.ran@gmail.com>
2017-06-22 15:56:06 +0530

    Master Commit

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
+    var tweet;
+
+    var getQuote = function (data) {
+        $(".quote").text(data.quoteText);
+        if (data.quoteAuthor === '') {
+            data.quoteAuthor = 'Unknown';
+        }
+        tweet = 'https://twitter.com/intent/tweet?text=' + data.quoteText + '-- ' + data.quoteAuthor +
+            ' @vikispeaks';
+        $(".author").text('--' + data.quoteAuthor);
+    };
+
+    var getRandomColor = function () {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    var setRandomColor = function () {
+        $(".bg").css("background", getRandomColor());
+        $("h2").css("background", getRandomColor());
+    }
+
+    var fetchQuote = function () {
+        $.getJSON(url, getQuote, 'jsonp');
+        setRandomColor();
+    };
+
+    fetchQuote();
+
+    $("#quote").click(function () {
+        fetchQuote();
+        setRandomColor();
+    });
+
+    $("#tweet-button").click(function () {
+        window.open(tweet, '_blank');
+    });
+});
